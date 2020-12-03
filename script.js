2fcdcce576f2da814cb25b5d967a5ddd083bf951
izzat <izzat_shafie88@yahoo.com>
2020-12-03 18:25:01 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let kira = document.getElementById("kira");
+let payment = document.getElementById("payment");
+let principal;
+let deposit;
+let interest;
+let years;
+
+kira.addEventListener("click", function calculateLoan() {
+  principal = document.getElementById("principal").value;
+  deposit = document.getElementById("deposit").value;
+  interest = document.getElementById("interest").value;
+  years = document.getElementById("years").value;
+
+  let totalPrinciple = principal-deposit;
+  let totalInterest = (interest/100)*totalPrinciple*years;
+  let totalToPay = totalInterest+totalPrinciple;
+  let monthlyToPay = totalToPay/years/12;
+  payment.innerHTML = "RM" + monthlyToPay.toFixed(0);
+})
